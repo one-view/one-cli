c9fdddbbaa601e6a7055abb99c722b063ef36fbf
roei <roeichow@gmail.com>
2018-03-02 13:38:24 +0800

    update: add web app template

---
+++ template/config/app/common.js
@@ -0,0 +1,7 @@
+// 应用配置
+
+const config = {
+  port: 6060
+}
+
+module.exports = config
---
+++ template/config/app/ftp.js
@@ -0,0 +1,33 @@
+
+const config = {
+
+  development: {
+    host: '',
+    user: '',
+    password: '',
+    origin: ''
+  },
+
+  qa: {
+    host: '',
+    user: '',
+    password: '',
+    origin: ''
+  },
+
+  prerelease: {
+    host: '',
+    user: '',
+    password: '',
+    origin: ''
+  },
+
+  release: {
+    host: '',
+    user: '',
+    password: '',
+    origin: ''
+  }
+}
+
+module.exports = config
